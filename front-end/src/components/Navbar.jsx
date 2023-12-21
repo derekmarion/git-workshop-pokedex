deb68a71a4f31391b35b8d8fd36b045867cd8be4
Matthew Peterson <matthew.s.peterson.39@gmail.com>
2023-12-21 12:23:30 -0500

    created moves page, link to navbar

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -6,6 +6,7 @@ export const Navbar = () => {
         <Row style={{display:"flex", justifyContent:"space-around"}}>
             <Link to="/">Home</Link>
             <Link to="pokemon">Pokemon</Link>
+            <Link to="moves">Moves</Link>
         </Row>
     )
 }
